d0a547da632ab0ff722d01da858e793f7fe5beb2
A-sleh <businessabdo9@gamil.com>
2023-10-01 18:24:46 +0300

    Add Funtion Merhge Sort

---
+++ Merge Sort/mergeSort.js
@@ -0,0 +1,48 @@
+// This Funtion To Sort Any Array By Merge Sort 
+let newArray ;
+
+function MergeSort(newArray,startPoint,mid,endPoint) {
+    let index = 0 ,
+        midll = mid - 1 ;
+        numberOfElements = endPoint - startPoint  + 1 ,
+        dis = startPoint ;
+        while( startPoint <= midll && mid <= endPoint ) {
+            if( sortArray[startPoint] < sortArray[mid] ) {
+                newArray[index++] = sortArray[startPoint++ ] ;
+            }else {
+                newArray[index++] = sortArray[mid++] ;
+            }
+        }
+        while(startPoint <= midll ) {
+            newArray[index++] = sortArray[startPoint++ ] ;
+        }
+        while( mid <= endPoint ) {
+            newArray[index++] = sortArray[mid++] ;
+        }
+        for(let i = 0 ; i < numberOfElements ; ++ i ) {
+            sortArray[dis + i ] = newArray[i];
+        }
+}
+
+function reMergeSort(newArray,startPoint,endPoint) {
+    if ( startPoint == endPoint ) {
+        return false;
+    } else {
+        let mid = parseInt((startPoint + endPoint ) / 2 );
+        reMergeSort(newArray,startPoint,mid);
+        reMergeSort(newArray,mid+1,endPoint);
+        MergeSort(newArray,startPoint,mid+1,endPoint) ;
+    }
+}// Complexity O(N * log(N))
+
+// Test Case 
+
+let sortArray = [ 8 , 9 , 10 , 3 , 2 , 1 , 5 , 4 ] ;
+
+console.log(`The Array Befor Sorted :`) ;
+console.log(sortArray)
+
+console.log(`The Array After Sorted :`) ;
+newArray = [...Array(sortArray.length)] ;// Create Empty Array
+reMergeSort(newArray , 0 , sortArray.length - 1) ;
+console.log(sortArray);
